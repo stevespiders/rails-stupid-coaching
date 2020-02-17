d4e17bc840f22f4a9a4a3fd745b10cd1f04bdaf1
Stefan Sideris <stefanpaulsideris@gmail.com>
2020-02-17 14:51:52 +0000

    finsihed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+
+    if @question == "I'm going to work"
+      @answer = 'Great!'
+    elsif @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
